ef34287e8ed28d284fadb89c3ce75f6dfeec440d
hc5bu <hc5bu@virginia.edu>
2021-02-18 19:18:57 -0500

    Hongze's First Commit

---
+++ hongze/index.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const Bot = new (require('discord.js').Client)();
+
+var TOKEN = process.env.DISCORDTOKEN;
+var NAME = process.env.DISCORDNAME;
+var CHANNEL = '812110768602480680'; // ID of the channel which this bot looks at
+
+Bot.sendMessage = (content, channel) => (Bot.channels.cache.get(channel ?? CHANNEL) ?? {send: () => null}).send(content);
+
+async function BotLogin(token, name, channel) {
+    if(token) TOKEN = token;
+    if(name) NAME= name;
+    if(channel) CHANNEL = channel;
+    await Bot.login(TOKEN);
+    console.log(`[DISC] Logged in as ${NAME}`);
+}
+
+Bot.on('error', err => {
+    console.log(`[DISC] !! Error in bot ${NAME}:\n{err}`);
+});
+
+// Dictates bot behavior when it sees a message being sent.
+async function onMessage(msg) {
+
+}
+
+// Behavior independent of messages goes here.
+// Function executed every 5 seconds.
+var time = 0;
+function onInterval(Client) {
+    // There's some interesting syntax going on here but your code goes inside the inner curly braces.
+    return async () => {
+        time += 5;
+        console.log('Executed interval.');
+    }
+}
+
+// Requires Nodejs v14.6.0
+// Runs only if this file is the main process.
+if(require.main === module) {
+    BotLogin();
+    Bot.on('message', msg => {
+        // Ignore messages from other bots
+        // Ignore messages from other channels
+        if(msg.author.bot || msg.channel.id !== CHANNEL) return;
+        onMessage(msg);
+    });
+
+    setInterval(onInterval(Bot), 5000);
+}
+
+module.exports = {
+    Bot: Bot,
+    Interval: onInterval,
+}

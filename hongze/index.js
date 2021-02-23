6d91449a26736f0aea990ca08c6f109515bb2e3c
hc5bu <hc5bu@virginia.edu>
2021-02-23 18:45:31 -0500

    Everything else

--- hongze/index.js
+++ hongze/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Bot = new (require('discord.js').Client)();
+const fetch = require('node-fetch');
 
 var TOKEN = process.env.DISCORDTOKEN;
 var NAME = process.env.DISCORDNAME;
@@ -22,8 +23,24 @@ Bot.on('error', err => {
 // Dictates bot behavior when it sees a message being sent.
 async function onMessage(msg) {
   if(msg.content.toUpperCase() === 'PING')
-    msg.channel.send('Ping')
+    msg.channel.send('Pong');
+  else if(msg.content.toLowerCase() === 'hi')
+    msg.channel.send('Hi DEVHUB! How is everyone?');
+  else if(msg.content.toLowerCase().startsWith('tell me about yourself'))
+    msg.channel.send("My name is Hongze Chen, and I\'m a second-year majoring in mathematics and minoring in CS. "+
+    "Like most people I\'m slowly going insane during the pandemic, but I generally enjoy assembling and fixing things, "+
+    "the internet, traveling, etc. I am easygoing, but also extremely meticulous and perfectionistic, especially when it "+
+    "comes to working with software. I hope to be a helpful and dedicated teammate to everyone throughout this semester.");
+  else if(msg.content.toLowerCase().startsWith("what's the weather")){
+    //is there a way to conceal my API code?
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=Charlottesville,US&units=imperial&appid=52520c6b8e3b1737918c28a63960625d").then(
+      r => r.json()).then(data => {
+        const s = `Right now the weather in Charlottesville is ${data.weather[0].main.toLowerCase()}, and the temperature is ${data.main.temp}°F.`;
+        msg.channel.send(s);
+      }, r => msg.channel.send('Whoops! Something went wrong during the API fetch. Try again later'))
+  }
 }
+
 
 // Behavior independent of messages goes here.
 // Function executed every 5 seconds.
